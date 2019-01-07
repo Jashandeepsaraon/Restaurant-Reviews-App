83d751be13e3df4260ad2e9892988f1c6a70e397
Jashan Saraon <Jashanp.k99@gmail.com>
2019-01-07 11:14:33 -0600

    Fix the bugs.

--- sw.js
+++ sw.js
@@ -19,24 +19,48 @@ self.addEventListener("install", function(event) {
         caches.open(staticCache).then(function(cache) {
             return cache.addAll(
                 [
-                    "./img/1.jpg",
-                    "./img/2.jpg",
-                    "./img/3.jpg",
-                    "./img/4.jpg",
-                    "./img/5.jpg",
-                    "./img/6.jpg",
-                    "./img/7.jpg",
-                    "./img/8.jpg",
-                    "./img/9.jpg",
-                    "./img/10.jpg",
+                    "/img/1.jpg",
+                    "/img/2.jpg",
+                    "/img/3.jpg",
+                    "/img/4.jpg",
+                    "/img/5.jpg",
+                    "/img/6.jpg",
+                    "/img/7.jpg",
+                    "/img/8.jpg",
+                    "/img/9.jpg",
+                    "/img/10.jpg",
                     "./js/main.js",
                     "./js/dbhelper.js",
                     "./js/restaurant_info.js",
                     "./css/styles.css",
                     "./",
-                    "./restaurant.html",
+                    "/restaurant.html",
                     "./index.html",
-                    "./data/restaurants.json"
+                    "/data/restaurants.json",
+                    './restaurant.html?id=1',
+                    './restaurant.html?id=2',
+                    './restaurant.html?id=3',
+                    './restaurant.html?id=4',
+                    './restaurant.html?id=5',
+                    './restaurant.html?id=6',
+                    './restaurant.html?id=7',
+                    './restaurant.html?id=8',
+                    './restaurant.html?id=9',
+                    './restaurant.html?id=10',
+                    "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
+                    "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
+                    "http://localhost:5500/data/restaurants.json",
+                    "http://127.0.0.1:5500/manifest.json",
+                    "https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png",
+                    "https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw ",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1539.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1539.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1540.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
+                    "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1540.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw",
                 ]
             )
         })
@@ -47,8 +71,8 @@ self.addEventListener('fetch', function(event) {
     event.respondWith(
         caches.match(event.request).then(function(response) {
             return response || fetch(event.request)
-        }).catch(function(err){
+        })/*.catch(function(err){
             console.log("fetch error" , err)
-        })
+        })*/
     )
 });
