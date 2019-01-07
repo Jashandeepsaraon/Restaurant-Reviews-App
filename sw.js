e756ab618bf79721929e6a32d2fa182fa916fbd7
Jashan Saraon <Jashanp.k99@gmail.com>
2019-01-07 12:12:44 -0600

    Update

--- sw.js
+++ sw.js
@@ -49,8 +49,7 @@ self.addEventListener("install", function(event) {
                     './restaurant.html?id=10',
                     "https://unpkg.com/leaflet@1.3.1/dist/leaflet.css",
                     "https://unpkg.com/leaflet@1.3.1/dist/leaflet.js",
-                    "http://localhost:5500/data/restaurants.json",
-                    "http://127.0.0.1:5500/manifest.json",
+                    
                     "https://unpkg.com/leaflet@1.3.1/dist/images/marker-shadow.png",
                     "https://unpkg.com/leaflet@1.3.1/dist/images/marker-icon.png",
                     "https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoiamFzaGFuZGVlcCIsImEiOiJjanB5N3N1ZjgweTdqNDNxbHg3NXpxMGF2In0.4sCdGCrRogMI2yP7q2-IPw ",
